d2be125d8889bfb72d864e3262956f1c97514b61
Pete Brown <psychlist1972@outlook.com>
2023-03-23 16:28:39 -0400

    Update MidiUmpEndpoint.cpp

--- src/api-client/Windows.Devices.Midi2/MidiUmpEndpoint.cpp
+++ src/api-client/Windows.Devices.Midi2/MidiUmpEndpoint.cpp
@@ -5,6 +5,11 @@
 
 namespace winrt::Windows::Devices::Midi2::implementation
 {
+    // TODO: these will be broken out into interfaces
+    // IInputUmpEndpoint            : for mapping MIDI 1.0 input-only byte stream devices to UMP
+    // IOutputUmpEndpoint           : for mapping MIDI 1.0 output-only byte stream devices to UMP
+    // IBidirectionalUmpEndpoint    : approach for native MIDI 2.0 devices and MIDI 1.0 devices with in and out
+
     winrt::Windows::Devices::Midi2::MidiMessageReadBuffer MidiUmpEndpoint::IncomingMessages()
     {
         throw hresult_not_implemented();
@@ -14,7 +19,6 @@ namespace winrt::Windows::Devices::Midi2::implementation
         throw hresult_not_implemented();
     }
 }
-
 
 
 // Prototype: Hard-code creating a new network MIDI endpoint
